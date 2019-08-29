3a6cdfafe8bf5e96ae6ba3a2a840e49cf9dbf4d5
Brian Kao <bk1613@nyu.edu>
2019-08-29 15:02:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-row1 = [" ", "|", " ", "|", " "]
-dashline = "-------------"
-row2 = [" ", "|", " ", "|", " "]
-dashline2 = "-------------"
-row3 = [" ", "|", " ", "|", " "]
+
 
 
 
